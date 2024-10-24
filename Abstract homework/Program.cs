4ddc61cee60458b179b9f0ffb08cd43dcef71ac2
CallMeNAKS <thefan51999@gmail.com>
2024-10-24 15:48:53 +0300

    Initial commit

---
+++ Abstract homework/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    public static void Main()
+    {
+        var car = new Car();
+        var ship = new Ship();
+        var plane = new Plane();
+     
+        car.Go();
+        ship.Go();
+        plane.Go();
+    }
+}
+
+public abstract class Vehicle
+{
+    public abstract void Go();
+}
+
+public class Car : Vehicle
+{
+    public override void Go()
+    {
+        Console.WriteLine("Car go");
+    }
+}
+public class Ship : Vehicle
+{
+    public override void Go()
+    {
+        Console.WriteLine("Ship go");
+    }
+}
+public class Plane : Vehicle
+{
+    public override void Go()
+    {
+        Console.WriteLine("Plane go");
+    }
+}
